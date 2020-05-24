3dea0c7a86fac592b6e0120ac91321ffe7c67264
Amardip Kaur <akaur66@mail.greenriver.edu>
2020-05-24 16:17:52 -0400

    added personal information form validation

--- controllers/controller.php
+++ controllers/controller.php
@@ -24,54 +24,71 @@ class Controller
      */
     public function home()
     {
-        //echo '<h1>Welcome to my Food Page</h1>';
-
         $view = new Template();
         echo $view->render('views/home.html');
     }
 
     /**
-     * Process the order route
+     * Process the personal information route
      */
     public function order()
     {
         //If the form has been submitted
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            //Validate food
-            /*if (!$this->_validator->validFood($_POST['food'])) {
+            //Validate //if data is not valid
+            if (!$this->_validator->validName($_POST['firstName'])) {
 
                 //Set an error variable in the F3 hive
-                $this->_f3->set('errors["food"]', "Invalid food item");
+                $this->_f3->set('errors["firstName"]', "Please enter your first name");
             }
-            if (!$this->_validator->validMeal($_POST['meal'])) {
+            if (!$this->_validator->validName($_POST['lastName'])) {
 
                 //Set an error variable in the F3 hive
-                $this->_f3->set('errors["meal"]', "Invalid meal.");
+                $this->_f3->set('errors["lastName"]', "Please enter your last name");
+            }
+            if (!$this->_validator->validAge($_POST['age'])) {
+
+                //Set an error variable in the F3 hive
+                $this->_f3->set('errors["age"]', "Enter correct age. Must be 18 or older");
+            }
+            if (!$this->_validator->validPhone($_POST['phone'])) {
+
+                //Set an error variable in the F3 hive
+                $this->_f3->set('errors["phone"]', "Please enter correct number");
+            }
+            if (!$this->_validator->validEmail($_POST['email'])) {
+
+                //Set an error variable in the F3 hive
+                $this->_f3->set('errors["email"]', "Correct email with @ and . required");
             }
             //Data is valid
             if (empty($this->_f3->get('errors'))) {
 
-                //Create an order object
-                $order = new FoodOrder();
-                $order->setFood($_POST['food']);
-                $order->setMeal($_POST['meal']);
+                //Create object   ////saved in session for now ////
+                $_SESSION['firstName'] = $_POST['firstName'];
+                $_SESSION['lastName'] = $_POST['lastName'];
+                $_SESSION['age'] = $_POST['age'];
+                $_SESSION['phone'] = $_POST['phone'];
+                $_SESSION['email'] = $_POST['email'];
 
-                //Store the object in the session array
-                $_SESSION['order'] = $order;
+                //Redirect to Vehicle Info form page
+                $this->_f3->reroute('vehicleForm');
 
-                //Redirect to Order 2 page
-                $this->_f3->reroute('order2');
+//                $order = new FoodOrder();
+//                $order->setFood($_POST['food']);
+//                $order->setMeal($_POST['meal']);
+//
+//                //Store the object in the session array
+//                $_SESSION['order'] = $order;
             }
         }
 
-        $this->_f3->set('meals', getMeals());
-        $this->_f3->set('food', $_POST['food']);
-        $this->_f3->set('selectedMeal', $_POST['meal']);
-        */
-
-        $this->_f3->reroute('vehicleForm');
-}
+        $this->_f3->set('firstName', $_POST['firstName']);
+        $this->_f3->set('lastName', $_POST['lastName']);
+        $this->_f3->set('phone', $_POST['phone']);
+        $this->_f3->set('age', $_POST['age']);
+        $this->_f3->set('email', $_POST['email']);
 
         $view = new Template();
         echo $view->render('views/orderForm.html');
--- model/validate.php
+++ model/validate.php
@@ -3,28 +3,57 @@
 /**
  * Class Validate
  * Contains the validation methods for my app
- * @author Tina Ostrander
+ * @author Amardip Kaur
+ * @author John Laygo
+ * @author Michael Gulchuk
  * @version 1.0
  */
 class Validate
 {
-    /* Return a value indicating if food parameter is valid
-       Valid foods are not empty and do not contain anything except letters
-       @param String $food
+    /* Return a value indicating if name parameter is valid
+       Valid name is not empty and do not contain anything except letters
+       @param String $name
        @return boolean
     */
-    function validFood($food)
+    function validName($name)
     {
-        /*
-        if(!empty($food) && ctype_alpha($food)) {
-            return true;
-        } else {
-            return false;
-        }
-        */
+        $name = str_replace(' ', '', $name); //remove white space
+        //not empty         //all alphabets
+        return !empty($name) && ctype_alpha($name);
+    }
 
-        $food = str_replace(' ', '', $food);
-        return !empty($food) && ctype_alpha($food);
+    /* Return a value indicating if age is valid
+       Valid age is being older than 18 and all numeric
+       @param String $age
+       @return boolean
+    */
+    function validAge($age)
+    {
+        //not empty         //numeric           //between 18 and 118
+        return !empty($age) && is_numeric($age) && ($age >= 18 && $age <= 120);
+    }
+
+    /* Return a value indicating if phone number is valid
+       Valid phone is 10 letter or more and all numeric
+       @param String $number
+       @return boolean
+    */
+    function validPhone($number)
+    {
+        $number = str_replace(' ', '', $number); //remove white space
+        //10 characters or more         //numeric
+        return (strlen($number) >= 10 && is_numeric($number));  // 1 if true
+    }
+
+    /* Return a value indicating if email is valid
+       Valid email includes @ and .
+       @param String $email
+       @return boolean
+    */
+    function validEmail($email)
+    {
+        $number = str_replace(' ', '', $email); //remove white space
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
     /* Return a value indicating if meal is valid
