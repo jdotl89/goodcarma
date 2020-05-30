e5d208e9d44b369ebc59241d2dbd1e64ed47529e
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-30 02:35:52 -0400

    initialized parent class file vehicle.php

---
+++ classes/vehicle.php
@@ -0,0 +1,158 @@
+<?php
+
+class Vehicle
+{
+    //Declare instance variables
+    private $_year;
+    private $_make;
+    private $_model;
+    private $_engine;
+    private $_transmission;
+    private $_color;
+
+    /** Default constructor
+     * @param $year the year
+     * @param $make the make
+     * @param $model the model
+     * @param $engine the engine
+     * @param $transmission the transmission
+     * @param $color the color
+     */
+    public function __construct($year = "2000",
+                                $make = "bmw",
+                                $model = "sedan",
+                                $engine = "4 cycliner",
+                                $transmission = "automatic",
+                                $color = "red")
+    {
+        $this->setYear($year);
+        $this->setMake($make);
+        $this->setModel($model);
+        $this->setEngine($engine);
+        $this->setTransmission($transmission);
+        $this->setColor($color);
+    }
+
+    /** Set the year
+     *  @param $year the year
+     */
+    public function setYear($year)
+    {
+        $this->_year = $year;
+    }
+
+    /** Get the year
+     *  @return the year
+     */
+    public function getYear()
+    {
+        return $this->_year;
+    }
+
+    /**
+ * @param string the $make
+ */
+    public function setMake($make)
+    {
+        $this->_make = $make;
+    }
+
+    /**
+     * @return string the make
+     */
+    public function getMake()
+    {
+        return $this->_make;
+    }
+
+    /**
+     * @param string the $model
+     */
+    public function setModel($model)
+    {
+        $this->_model = $model;
+    }
+
+    /**
+     * @return string the model
+     */
+    public function getModel()
+    {
+        return $this->_model;
+    }
+
+    /**
+     * @param string the $engine
+     */
+    public function setEngine($engine)
+    {
+        $this->_engine = $engine;
+    }
+
+    /**
+     * @return string the engine
+     */
+    public function getEngine()
+    {
+        return $this->_engine;
+    }
+
+    /**
+     * @param string the $transmission
+     */
+    public function setTransmission($transmission)
+    {
+        $this->_transmission = $transmission;
+    }
+
+    /**
+     * @return string the transmission
+     */
+    public function getTransmission()
+    {
+        return $this->_transmission;
+    }
+
+    /**
+     * @param string the $color
+     */
+    public function setColor($color)
+    {
+        $this->_color = $color;
+    }
+
+    /**
+     * @return string the color
+     */
+    public function getColor()
+    {
+        return $this->_color;
+    }
+
+    /** toString() returns a String representation
+     *  of an order object
+     *  @return string
+     */
+    public function toString()
+    {
+        $out = $this->_food . ", ";
+        $out .= $this->_meal . ", ";
+
+        if (!empty($this->_condiments)) {
+            $out .= implode(" & ", $this->_condiments);
+        }
+
+        return $out;
+    }
+}
+
+/* For testing purposes only */
+/*
+$order = new FoodOrder("pizza", "lunch", array("parmesan", "red pepper flakes"));
+echo $order->toString() . "<br>";
+$order2 = new FoodOrder();
+echo $order2->toString() . "<br>";
+$order3 = new FoodOrder("tacos", "dinner");
+$order3->setCondiments(array("taco sauce", "sour cream"));
+echo $order3->toString() . "<br>";
+*/
